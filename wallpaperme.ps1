2470588c497922bd7ab103e3d01ee7e4dc4ae396
appetiteoven <AppetiteOvenLLC@outlook.com>
2013-11-18 21:50:43 -0800

    Update wallpaperme.ps1

--- wallpaperme.ps1
+++ wallpaperme.ps1
@@ -4,13 +4,13 @@ Function Set-WallPaper($Value)
  rundll32.exe user32.dll, UpdatePerUserSystemParameters
 }
  
-
 $source = "http://ibankcoin.com/chessnwine/files/2013/10/lumberg.jpg"
 $Filename = [System.IO.Path]::GetFileName($source)
 $dest = "$env:temp\lumberg.jpg"
 
 $wc = New-Object System.Net.WebClient
 $wc.DownloadFile($source, $dest)
-
+write-host "Setting wallpaper to $dest"
 Set-WallPaper $dest
+ 
  
