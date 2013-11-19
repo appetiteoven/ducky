97da6c65694c691b1ac62276c3215b4e6661a7a8
appetiteoven <AppetiteOvenLLC@outlook.com>
2013-11-18 21:47:56 -0800

    Update wallpaperme.ps1

--- wallpaperme.ps1
+++ wallpaperme.ps1
@@ -1,1 +1,16 @@
-write-host "wallpaper me"
+Function Set-WallPaper($Value)
+{
+ Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value $value
+ rundll32.exe user32.dll, UpdatePerUserSystemParameters
+}
+ 
+
+$source = "http://ibankcoin.com/chessnwine/files/2013/10/lumberg.jpg"
+$Filename = [System.IO.Path]::GetFileName($source)
+$dest = "$env:temp\lumberg.jpg"
+
+$wc = New-Object System.Net.WebClient
+$wc.DownloadFile($source, $dest)
+
+Set-WallPaper $dest
+ 
