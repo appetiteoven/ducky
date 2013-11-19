4b0ef89e07a718bacd188a12f01e96dde37bb31c
appetiteoven <AppetiteOvenLLC@outlook.com>
2013-11-18 22:04:24 -0800

    Update wallpaperme.ps1

--- wallpaperme.ps1
+++ wallpaperme.ps1
@@ -1,16 +1,13 @@
-Function Set-WallPaper($Value)
-{
- Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value $value
- rundll32.exe user32.dll, UpdatePerUserSystemParameters
-}
- 
-$source = "http://ibankcoin.com/chessnwine/files/2013/10/lumberg.jpg"
-$Filename = [System.IO.Path]::GetFileName($source)
-$dest = "$env:temp\lumberg.jpg"
-
 $wc = New-Object System.Net.WebClient
-$wc.DownloadFile($source, $dest)
+$wallpaper_url = "http://ibankcoin.com/chessnwine/files/2013/10/lumberg.jpg"
+$dest = "$env:temp\lumberg_wallpaper.jpg"
+
+$wc.DownloadFile($wallpaper_url, $dest)
+
 write-host "Setting wallpaper to $dest"
-Set-WallPaper $dest
- 
+Set-ItemProperty -path "HKCU:\Control Panel\Desktop\" -name wallpaper -value $dest
+RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters ,2 ,True
+rundll32.exe user32.dll, UpdatePerUserSystemParameters
+
+
  
